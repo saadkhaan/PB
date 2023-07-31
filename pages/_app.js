283f07b38369af2ce2bd0ca39353744515b20f65
saadkhaan <saad@pixelsbit.com>
2023-07-31 11:12:29 +0500

    updates to the portfolio and seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
+	const p = router.asPath.slice(1);
+	const canonicalURL = `https://example.com/${p}`.split("?")[0];
+
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Head>
+				<link rel="canonical" href={canonicalURL} />
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	);
 }
--- pages/work/index.js
+++ pages/work/index.js
@@ -28,88 +28,278 @@ export default function work() {
 							<span className="font-normal">Here are a few highlights</span>.
 						</p>
 					</div>
-					<div className="mt-8 grid grid-cols-1 gap-x-4 gap-y-16 md:grid-cols-2">
-						<div className="card flex flex-col ">
-							<div className="card-bg relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max before:origin-top-left  before:scale-x-75 before:transform before:rounded-lg before:bg-gray-800 before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
-								<div className="card__image relative">
-									<img src="/car.png" alt="" className="w-auto md:w-auto" />
-								</div>
-							</div>
-							<div className="card__details">
-								<div className="card__details-header group flex  items-center">
-									<h3 className="text-xl text-gray-800 md:text-2xl">
-										<a
-											href="https://limousine4less.us"
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											Limousine 4 Less
-										</a>
-									</h3>
-									<svg
-										width="16"
-										height="10"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-										className="ml-2 transform transition-all duration-200 group-hover:ml-4"
-									>
-										<path
-											fillRule="evenodd"
-											clipRule="evenodd"
-											d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
-											fill="#1F2937"
+					<div className="portfolio-container mt-12 md:mt-20">
+						<h3 className="block text-2xl font-semibold md:mb-20 md:text-center md:text-5xl">
+							Corporate Websites
+						</h3>
+						<div className="mt-8 grid grid-cols-1 gap-x-4 gap-y-16 md:grid-cols-2">
+							<div className="card flex flex-col">
+								<div className="card-bg relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max before:origin-top-left  before:scale-x-75 before:transform before:rounded-lg before:bg-[#000A13] before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
+									<div className="card__image relative">
+										<img
+											src="/mic.png"
+											alt="Podcast website design and web development service"
+											className="mx-auto -mt-px w-20 md:-mt-16 md:w-auto"
 										/>
-									</svg>
+									</div>
 								</div>
-
-								<p className="text-base text-gray-400 md:text-lg">
-									Website Design, Web Development and Social Media Marketing
-								</p>
-							</div>
-						</div>
-
-						<div className="card flex flex-col">
-							<div className="card-bg relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max before:origin-top-left  before:scale-x-75 before:transform before:rounded-lg before:bg-[#000A13] before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
-								<div className="card__image relative">
-									<img
-										src="/mic.png"
-										alt=""
-										className="mx-auto -mt-px w-20 md:-mt-16 md:w-auto"
-									/>
-								</div>
-							</div>
-							<div className="card__details">
-								<div className="card__details-header group flex  items-center">
-									<h3 className="text-xl text-gray-800 md:text-2xl">
-										<a
-											href="https://conversationswithloulou.com"
-											target="_blank"
-											rel="noopener noreferrer"
+								<div className="card__details">
+									<div className="card__details-header group flex  items-center">
+										<h3 className="text-xl text-gray-800 md:text-2xl">
+											<a
+												href="https://conversationswithloulou.com"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												Conversations with Loulou
+											</a>
+										</h3>
+										<svg
+											width="16"
+											height="10"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+											className="ml-2 transform transition-all duration-200 group-hover:ml-4"
 										>
-											Conversations with Loulou
-										</a>
-									</h3>
-									<svg
-										width="16"
-										height="10"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-										className="ml-2 transform transition-all duration-200 group-hover:ml-4"
-									>
-										<path
-											fillRule="evenodd"
-											clipRule="evenodd"
-											d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
-											fill="#1F2937"
-										/>
-									</svg>
+											<path
+												fillRule="evenodd"
+												clipRule="evenodd"
+												d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
+												fill="#1F2937"
+											/>
+										</svg>
+									</div>
+									<p className="text-base text-gray-400 md:text-lg">
+										Website Design and Web Development
+									</p>
 								</div>
-								<p className="text-base text-gray-400 md:text-lg">
-									Website Design and Web Development
-								</p>
 							</div>
+							<div className="card flex flex-col">
+								<div className="card-bg relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max before:origin-top-left  before:scale-x-75 before:transform before:rounded-lg before:bg-[#041243] before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
+									<div className="card__image relative">
+										<img
+											src="/card.png"
+											alt="Edenred Dubai web development"
+											className="mx-auto -mt-px w-44 md:-mt-12 md:w-auto"
+										/>
+									</div>
+								</div>
+								<div className="card__details">
+									<div className="card__details-header group flex  items-center">
+										<h3 className="text-xl text-gray-800 md:text-2xl">
+											<a
+												href="https://edenred.ae/"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												Edenred
+											</a>
+										</h3>
+										<svg
+											width="16"
+											height="10"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+											className="ml-2 transform transition-all duration-200 group-hover:ml-4"
+										>
+											<path
+												fillRule="evenodd"
+												clipRule="evenodd"
+												d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
+												fill="#1F2937"
+											/>
+										</svg>
+									</div>
+									<p className="text-base text-gray-400 md:text-lg">
+										Website Design and Web Development
+									</p>
+								</div>
+							</div>
+
+							<div className="card flex flex-col">
+								<div className="card-bg before:bg-neutral-950 relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max  before:origin-top-left before:scale-x-75 before:transform before:rounded-lg before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
+									<div className="card__image relative">
+										<img
+											src="/spade-logo.svg"
+											alt="Ventures website"
+											className="w-md-80 mx-auto mt-8 h-auto w-48 md:mt-16"
+										/>
+									</div>
+								</div>
+								<div className="card__details">
+									<div className="card__details-header group flex items-center">
+										<h3 className="text-xl text-gray-800 md:text-2xl">
+											<a
+												href="https://spade.ventures/"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												Spade Ventures
+											</a>
+										</h3>
+										<svg
+											width="16"
+											height="10"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+											className="ml-2 transform transition-all duration-200 group-hover:ml-4"
+										>
+											<path
+												fillRule="evenodd"
+												clipRule="evenodd"
+												d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
+												fill="#1F2937"
+											/>
+										</svg>
+									</div>
+									<p className="text-base text-gray-400 md:text-lg">
+										Web Design and Web Development
+									</p>
+								</div>
+							</div>
+
+							{/* KGV */}
+							<div className="card flex flex-col ">
+								<div className="card-bg relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max before:origin-top-left  before:scale-x-75 before:transform before:rounded-lg before:bg-neutral-900 before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
+									<div className="card__image relative">
+										<img
+											src="/kgv-logo.svg"
+											alt="Web design and development for a group of companies"
+											className="w-md-60 mx-auto mt-6 h-auto w-32 md:mt-16"
+										/>
+									</div>
+								</div>
+								<div className="card__details">
+									<div className="card__details-header group flex  items-center">
+										<h3 className="text-xl text-gray-800 md:text-2xl">
+											<a
+												href="https://kgv.ae"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												KGV
+											</a>
+										</h3>
+										<svg
+											width="16"
+											height="10"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+											className="ml-2 transform transition-all duration-200 group-hover:ml-4"
+										>
+											<path
+												fillRule="evenodd"
+												clipRule="evenodd"
+												d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
+												fill="#1F2937"
+											/>
+										</svg>
+									</div>
+									<p className="text-base text-gray-400 md:text-lg">
+										Web Design, Web Development
+									</p>
+								</div>
+							</div>
+							{/* KGV */}
 						</div>
 					</div>
+
+					{/* Section 2 */}
+					<div className="row mt-28">
+						<h3 className="block text-2xl font-semibold md:mb-20 md:text-center md:text-5xl">
+							Car Rental / Limousine Services Websites
+						</h3>
+						<div className="mt-8 grid grid-cols-1 gap-x-4 gap-y-16 md:grid-cols-2">
+							{/* Limousine 4 Less */}
+							<div className="card flex flex-col ">
+								<div className="card-bg relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max before:origin-top-left  before:scale-x-75 before:transform before:rounded-lg before:bg-[#BB133E] before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
+									<div className="card__image relative">
+										<img
+											src="/limo-front-view.png"
+											alt="Limo service and car rental web design and web development"
+											className="mx-auto w-auto md:-mt-12 md:w-auto"
+										/>
+									</div>
+								</div>
+								<div className="card__details">
+									<div className="card__details-header group flex  items-center">
+										<h3 className="text-xl text-gray-800 md:text-2xl">
+											<a
+												href="https://limousine4less.us"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												Limousine 4 Less
+											</a>
+										</h3>
+										<svg
+											width="16"
+											height="10"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+											className="ml-2 transform transition-all duration-200 group-hover:ml-4"
+										>
+											<path
+												fillRule="evenodd"
+												clipRule="evenodd"
+												d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
+												fill="#1F2937"
+											/>
+										</svg>
+									</div>
+									<p className="text-base text-gray-400 md:text-lg">
+										Website Design, Web Development and Social Media Marketing
+									</p>
+								</div>
+							</div>
+							{/* Limousine 4 Less */}
+
+							{/* Black Cab Houston */}
+							<div className="card flex flex-col ">
+								<div className="card-bg before:bg-neutral-950 relative h-[200px] px-3 pt-6 before:absolute before:-inset-1 before:block before:h-48 before:max-h-max  before:origin-top-left before:scale-x-75 before:transform before:rounded-lg before:transition before:duration-500 before:ease-[cubic-bezier(0.79,0.14,0.15,0.86)] before:hover:scale-x-100 md:h-[300px] md:before:h-72">
+									<div className="card__image relative">
+										<img
+											src="/car.png"
+											alt="Black Cab Houston"
+											className="w-auto md:w-auto"
+										/>
+									</div>
+								</div>
+								<div className="card__details">
+									<div className="card__details-header group flex  items-center">
+										<h3 className="text-xl text-gray-800 md:text-2xl">
+											<a
+												href="https://blackcabhouston.com/"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												Black Cab Houston
+											</a>
+										</h3>
+										<svg
+											width="16"
+											height="10"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+											className="ml-2 transform transition-all duration-200 group-hover:ml-4"
+										>
+											<path
+												fillRule="evenodd"
+												clipRule="evenodd"
+												d="M10.293.293a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414-1.414L12.586 6H1a1 1 0 0 1 0-2h11.586l-2.293-2.293a1 1 0 0 1 0-1.414Z"
+												fill="#1F2937"
+											/>
+										</svg>
+									</div>
+									<p className="text-base text-gray-400 md:text-lg">
+										Website Design, Web Development and Social Media Marketing
+									</p>
+								</div>
+							</div>
+							{/* Black Cab Houston */}
+						</div>
+					</div>
+					{/* Section 2 */}
 				</div>
 			</section>
 		</>
